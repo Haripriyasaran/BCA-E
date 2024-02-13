e472ce40dc64cf005772a1264519a19c163e413c
vandana64 <148611129+vandana64@users.noreply.github.com>
2024-02-13 14:58:05 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -1,6 +1,5 @@
 public class mul{
     public static void main (string args[]){
-        System.out.print("new file in mul java");
         System.out.println("hello");
         System.out.print( "hi");
     }
