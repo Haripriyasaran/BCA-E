163a2f3f2d8bb1b214d56c6c8c0acb519c8658a5
vandana64 <148611129+vandana64@users.noreply.github.com>
2024-02-13 15:05:12 +0530

    Update mul.java

--- mul.java
+++ mul.java
@@ -2,5 +2,6 @@ public class mul{
     public static void main (string args[]){
         System.out.println("hello");
         System.out.print( "hi");
+        System.out. print("hello world");
     }
 }
