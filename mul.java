2b56766302243718f4569ce1a7bc91e216498a10
vandana64 <vandanasaharan11@gmail.com>
2024-02-13 14:50:29 +0530

    hello

---
+++ mul.java
@@ -0,0 +1,7 @@
+public class mul{
+    public static void main (string args[]){
+        System.out.print("new file in mul java");
+        System.out.println("hello");
+        System.out.print( "hi");
+    }
+}
